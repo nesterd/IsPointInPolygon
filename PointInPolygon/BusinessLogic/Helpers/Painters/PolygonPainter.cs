c8556061a0a6c8ccdb9c419411166fd987caf8c4
nesterd <nesterd85@gmail.com>
2017-02-26 17:09:21 +0700

    Update PolygonPainter.cs

--- PointInPolygon/BusinessLogic/Helpers/Painters/PolygonPainter.cs
+++ PointInPolygon/BusinessLogic/Helpers/Painters/PolygonPainter.cs
@@ -18,31 +18,8 @@ namespace BusinessLogic.Helpers.Painters
     {
         static IList<string> _filePathToDeleteList = new List<string>();
 
-        //static string _fileExtention = ".png";
-        //static string _filePathPolygon = HostingEnvironment.MapPath("~/images/PolygonImage.png");
-        //static string _filePathBaseImage = HostingEnvironment.MapPath("~/images/Base/BaseImage.png");
-        //static string _filePathPolygonWithPoint;
-        //static string _fileName; 
-        //static int count = 1;
-        //const int ImageSize0X = 320;
-        //const int ImageSize0Y = 240;
-
-        //const int marginPolygon = 20;
-        //const int marginPoint = 5;
-
-        //public static int ImageSize0XForPolygon { get { return ImageSize0X - marginPolygon; } }
-        //public static int ImageSize0YForPolygon { get { return ImageSize0Y - marginPolygon; } }
-        //public static int ImageSize0XForPoint { get { return ImageSize0X - marginPoint; } }
-        //public static int ImageSize0YForPoint { get { return ImageSize0Y - marginPoint; } }
-        //public static string FilePathPolygon { get { return _filePathPolygon; } }
-        //public static string FilePathPolygonWithPoint { get { return _filePathPolygonWithPoint; } }
-        //public static string FileName { get { return _fileName; } }
-
-
         public string DrawPolygonWhileCrating(IEnumerable<Point> polygonVertices, string FileNameToDelete)
         {
-            
-
             var pen = new Pen(Color.Green);
 
             if (polygonVertices == null || polygonVertices.Count() <= 0)
@@ -61,25 +38,8 @@ namespace BusinessLogic.Helpers.Painters
 
         }
 
-        //public static void DrawPolygon(IEnumerable<Point> polygonVertices)
-        //{
-        //    DeleteImage(_filePathPolygon);
-
-        //    using (Bitmap image = new Bitmap(_filePathBaseImage))
-        //    {
-        //        using (Graphics graphic = Graphics.FromImage(image))
-        //        {
-        //            graphic.SmoothingMode = SmoothingMode.AntiAlias;
-        //            graphic.DrawPolygon(new Pen(Color.ForestGreen), polygonVertices.ToArray());
-        //        }
-                
-        //        image.Save(_filePathPolygon, ImageFormat.Png);
-        //    }
-        //}
-
         string BaseDrawPolygon(IEnumerable<Point> polygonVertices, Pen pen, Folder folderToSave, string filePathToChange)
         {
-            
             var fileName = GetRandomFileName();
             var filePath = GetFilePath(folderToSave, fileName);
             _filePathToDeleteList.Add(filePath);
@@ -90,32 +50,11 @@ namespace BusinessLogic.Helpers.Painters
                     graphic.SmoothingMode = SmoothingMode.AntiAlias;
                     graphic.DrawPolygon(pen, polygonVertices.ToArray());
                 }
-
                 image.Save(filePath, ImageFormat.Png);
             }
             return fileName;
         }
-
-        //public static void DrawPoint(Point point)
-        //{
-        //    DeleteImage(_filePathPolygonWithPoint);
-        //    _fileName = $"PolygonWithPointImage{count++}.png";
-        //    _filePathPolygonWithPoint = HostingEnvironment.MapPath($"~/images/{_fileName}");
-
-        //    if (File.Exists(_filePathPolygon))
-        //    {
-        //        using (Bitmap image = new Bitmap(_filePathPolygon))
-        //        {
-        //            using (Graphics graphic = Graphics.FromImage(image))
-        //            {
-        //                graphic.DrawRectangle(new Pen(Color.Red), point.X, point.Y, 1, 1);
-        //            }
-
-        //            image.Save(_filePathPolygonWithPoint, ImageFormat.Png);
-        //        }
-        //    }
-        //}
-
+        
         public string DrawPoint(Point point, string fileNameToChange)
         {
             var filePathToChange = GetFilePath(Folder.CreatingPolygon, fileNameToChange);
@@ -157,7 +96,6 @@ namespace BusinessLogic.Helpers.Painters
                     graphic.SmoothingMode = SmoothingMode.AntiAlias;
                     graphic.DrawLine(pen, point1, point2);
                 }
-
                 image.Save(filePath, ImageFormat.Png);
             }
             return fileName;
@@ -165,7 +103,6 @@ namespace BusinessLogic.Helpers.Painters
 
         void DeleteImage(string filePath)
         {
-            
             if (File.Exists(filePath))
                 File.Delete(filePath);
         }
@@ -186,7 +123,6 @@ namespace BusinessLogic.Helpers.Painters
 
             DeleteAllImages();
         }
-
 
         string GetFilePath(Folder folder, string fileName)
         {
